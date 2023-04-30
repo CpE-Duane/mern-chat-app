b2b1c0e3963774379768c5615c6ac31aad49a97a
Duane Villapando <villapando.duane@gmail.com>
2023-05-01 00:19:37 +0800

    asd

--- backend/index.js
+++ backend/index.js
@@ -27,11 +27,12 @@ app.use("/api/messages", messageRoutes)
 
 connectDB()
 
-// app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-// });
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+});
 
 const server = app.listen(process.env.PORT, () => {
      console.log(`Server started in PORT: ${process.env.PORT}`)
